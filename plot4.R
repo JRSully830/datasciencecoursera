ff43a53e639efa10eee5dd443bd7b0849158d22e
John R Sullivan <Jrsully830@comcast.net>
2016-04-16 08:48:39 -0400

    Exploratory analysis week 1 assignment

---
+++ plot4.R
@@ -0,0 +1,52 @@
+library(graphics);library(grDevices)
+
+##Read in data file
+DataDir <- "C:\\Users\\julienjohn13\\My Documents\\Data Science Courses\\Data\\"
+infile <- paste(DataDir,"houseconsump.txt",sep="")
+housCons <- read.table(infile,sep=";",stringsAsFactors=FALSE,skip=0,header=TRUE)
+
+## transform fields
+#housCons$Date <- as.Date(housCons$Date,format="%d/%m/%Y")
+
+housCons$Datetime <- paste(housCons$Date,housCons$Time)
+housCons$Datetime2 <- strptime(housCons$Datetime,format="%d/%m/%Y %H:%M:%S")
+housCons$Date <- as.Date(housCons$Date,format="%d/%m/%Y")
+housCons$Global_active_power <- as.numeric(housCons$Global_active_power)
+housCons$Global_reactive_power <- as.numeric(housCons$Global_reactive_power)
+housCons$Voltage <- as.numeric(housCons$Voltage)
+housCons$Global_intensity <- as.numeric(housCons$Global_intensity)
+housCons$Sub_metering_1 <- as.numeric(housCons$Sub_metering_1)
+housCons$Sub_metering_2 <- as.numeric(housCons$Sub_metering_2)
+housCons$Sub_metering_3 <- as.numeric(housCons$Sub_metering_3)
+
+ ##Split out data for 2 days
+housCons2 <- subset(housCons,Date=="2007-02-01"|Date=="2007-02-02",na.rm=TRUE)
+ 
+#open png device directly to address legend truncation
+png(paste0(DataDir,"plot4.png"),type="windows")
+
+
+par(mfrow=c(2,2),mar=c(4,4,3,1))
+## plot linegraph with legend to png device directly
+ with(housCons2, {
+                plot(Datetime2,Global_active_power,type="l",
+                    ylab="Global Active Power",xlab="")
+                
+                plot(Datetime2,Voltage,type="l",
+                    ylab="Voltage",xlab="datetime")
+                   
+                plot(Datetime2,Sub_metering_1,type="n",
+                     ylab="Energy Sub Metering",xlab="")
+                lines(Datetime2,Sub_metering_1,type="l",col="black")
+                lines(Datetime2,Sub_metering_2,type="l",col="red")
+                lines(Datetime2,Sub_metering_3,type="l",col="blue")
+                legend("topright",col=c("black","red","blue"),lty=1,bty="n",
+                  legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+                
+                plot(Datetime2,Global_reactive_power,type="l",
+                     xlab="datetime")
+                })
+                
+                
+ ## close device
+ dev.off()
